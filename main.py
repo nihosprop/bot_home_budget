0f376726a8254f1a2e7d9016d25c32aa4562521a
nihisprop <niwibonihs@tutamail.com>
2024-10-31 20:29:29 +0300

    feat(main.py): add from aiogram.client.default import DefaultBotProperties                    from aiogram.enums import ParseMode                    from config_data.config import Config, load_config

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import asyncio
 import logging
 
 from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config_data.config import Config, load_config
