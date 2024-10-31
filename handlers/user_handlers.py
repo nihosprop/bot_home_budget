1e9b2c4280ce1b205a100e431c4cf1e1533694f0
nihisprop <niwibonihs@tutamail.com>
2024-11-01 01:42:57 +0300

    feat(user_handlers.py): add handler async def cmd_help

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -6,3 +6,7 @@ from aiogram.types import CallbackQuery, Message
 logger = logging.getLogger(__name__)
 user_router = Router()
 
+
+@user_router.message(F.text.lower() == 'help')
+async def cmd_help(message: Message):
+    await message.answer('Это команда /help')
