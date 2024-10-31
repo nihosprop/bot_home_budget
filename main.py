67df941a819cfc05f6c464ec6ca298ca29418502
nihisprop <niwibonihs@tutamail.com>
2024-10-31 22:20:36 +0300

    feat(main.py): add from keyboards.set_menu import set_main_menu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram import Dispatcher, Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
-
+from keyboards.set_menu import set_main_menu
 logger = logging.getLogger(__name__)
 
 async def main():
