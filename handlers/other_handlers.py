28af5db88847d928de72f338cde4891b3c8544c4
nihisprop <niwibonihs@tutamail.com>
2024-11-01 00:47:26 +0300

    fix(async def answer_to_another): use lexicon.other_message in message.answer

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -7,8 +7,9 @@ from lexicon.lexicon_ru import LexiconRu
 
 logger = logging.getLogger(__name__)
 other_router = Router()
+lexicon = LexiconRu()
 
 @other_router.message()
 async def answer_to_another(message: Message):
     user_name = message.from_user.username or message.from_user.first_name
-    await message.answer(f'{user_name} {LEXICON_RU['other_message']}')
+    await message.answer(f'{user_name} {lexicon.other_message}')
