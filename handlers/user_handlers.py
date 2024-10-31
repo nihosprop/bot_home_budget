65e6aa327fc269cf620453debbb43e582a036687
nihisprop <niwibonihs@tutamail.com>
2024-11-01 01:36:03 +0300

    feat(user_handlers.py): add logger

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -3,5 +3,6 @@ from aiogram import Router, F
 from lexicon.lexicon_ru import LexiconRu
 from aiogram.types import CallbackQuery, Message
 
+logger = logging.getLogger(__name__)
 user_router = Router()
 
