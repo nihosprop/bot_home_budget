57229b6f1028bd6e82f07303a453d0c0b0f9d939
nihisprop <niwibonihs@tutamail.com>
2024-10-31 20:54:36 +0300

    feat(main.py): add logger                add async main function for bot initialization and polling

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ from aiogram import Dispatcher, Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
+
+logger = logging.getLogger(__name__)
+
+async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+                               '[%(asctime)s] - %(name)s - %(message)s',
+                        datefmt='%Y.%m.%d %H:%M:%S')
+    logger.info('Start bot')
+    config: Config = load_config()
+    bot = Bot(token=config.tg_bot.token,
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
