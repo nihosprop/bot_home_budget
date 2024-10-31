ff01f0184873eda0aef0f15d7b0c0c11f44d03db
nihisprop <niwibonihs@tutamail.com>
2024-10-31 23:55:05 +0300

    feat(other_handlers.py): add async def answer_to_another

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -5,3 +5,9 @@ from aiogram import Router
 
 logger = logging.getLogger(__name__)
 other_router = Router()
+
+@other_router.message()
+async def answer_to_another(message: Message):
+    user_name = message.from_user.username or message.from_user.first_name
+    await message.answer(f'{user_name}пишите по делу!')
+
