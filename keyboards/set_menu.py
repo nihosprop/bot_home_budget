bb7ebba74ccb6767b210988048ebea0916747fce
nihisprop <niwibonihs@tutamail.com>
2024-11-01 01:08:52 +0300

    refactor(set_menu.py): replace LEXICON_COMMANDS_RU with LexiconCommandsRu dataclass in set_main_menu

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -1,10 +1,10 @@
 from aiogram import Bot
 from aiogram.types import BotCommand
+from lexicon.lexicon_ru import LexiconCommandsRu
 
-from lexicon.lexicon_ru import LEXICON_COMMANDS_RU
-
+lexicon_cmd_ru = LexiconCommandsRu()
 
 async def set_main_menu(bot: Bot):
     main_menu_commands = [BotCommand(command=command, description=description)
-            for command, description in LEXICON_COMMANDS_RU.items()]
+            for command, description in lexicon_cmd_ru.__dict__.items()]
     await bot.set_my_commands(main_menu_commands)
