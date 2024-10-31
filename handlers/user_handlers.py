c7f0436809b756df8305d95a33db94033441237c
nihisprop <niwibonihs@tutamail.com>
2024-11-01 01:42:01 +0300

    fix(user_handlers.py): replace import LexiconRu with LexiconCommandRU

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,6 @@
 import logging
 from aiogram import Router, F
-from lexicon.lexicon_ru import LexiconRu
+from lexicon.lexicon_ru import LexiconCommandsRu
 from aiogram.types import CallbackQuery, Message
 
 logger = logging.getLogger(__name__)
