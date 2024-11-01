c214ab2873b2bacd6944763f62d68413dca433fc
nihisprop <niwibonihs@tutamail.com>
2024-11-01 23:58:47 +0300

    feat(user_handlers.py): add import InlineKeyboardBuilder

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,6 +2,7 @@ import logging
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
 from filters.filters import IsNumber
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu
 from aiogram.types import (CallbackQuery,
                            Message,
