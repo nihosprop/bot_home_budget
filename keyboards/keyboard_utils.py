92ed9fe6f4facdff96ed685b89a2a23d9dc0805d
nihisprop <niwibonihs@tutamail.com>
2024-11-02 00:44:10 +0300

    feat(keyboard_utils.py): add function create_inline_kb

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,5 +1,23 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from lexicon.lexicon_ru import Lexicon
+from lexicon.lexicon_ru import LEXICON
 
+def create_inline_kb(
+        width: int, *args: str, **kwargs: str) -> InlineKeyboardMarkup:
+    """Generates inline keyboards on the fly"""
 
+    kb_builder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(text=LEXICON[button] if
+            LEXICON.get(button) else button, callback_data=button))
+
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(text=text, callback_data=button))
+
+    kb_builder.row(*buttons, width=width)
+
+    return kb_builder.as_markup()
