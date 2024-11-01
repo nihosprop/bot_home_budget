0e82bbb192fa29af101fee8f68a15bceda861bd9
nihisprop <niwibonihs@tutamail.com>
2024-11-01 21:48:54 +0300

    style(lexicon_ru.py):

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,13 +1,13 @@
 import logging
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
+
 from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu
 from aiogram.types import CallbackQuery, Message, BotCommand
 
 
 logger = logging.getLogger(__name__)
 user_router = Router()
-lexicon_ru = LexiconRu()
 
 @user_router.message(CommandStart())
 async def cmd_start(message: Message):
