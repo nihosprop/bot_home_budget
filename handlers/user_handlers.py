f085e3254a52e6b3d5815600954cc053a5784778
nihisprop <niwibonihs@tutamail.com>
2024-11-02 00:24:11 +0300

    refactor(keyboard_utils.py): remove import InlineKeyboardBuilder

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,6 @@ import logging
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
 from filters.filters import IsNumber
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu
 from aiogram.types import (CallbackQuery,
                            Message,
