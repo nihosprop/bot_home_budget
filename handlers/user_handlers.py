c44ef511cfd6e864e81274aea6e48f61b6510c52
nihisprop <niwibonihs@tutamail.com>
2024-11-01 18:09:16 +0300

    feat(user_handlers.py): add command start handler and instance LexiconRu

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -7,6 +7,12 @@ from aiogram.types import CallbackQuery, Message, BotCommand
 
 logger = logging.getLogger(__name__)
 user_router = Router()
+lexicon = LexiconRu()
+
+@user_router.message(CommandStart())
+async def cmd_start(message: Message):
+    pass
+
 
 @user_router.message(F.text.lower() == '/help')
 async def cmd_help(message: Message):
