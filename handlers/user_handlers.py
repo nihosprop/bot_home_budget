096065f429bb1fbd21702407a52b85a7491f1d81
nihisprop <niwibonihs@tutamail.com>
2024-11-02 00:35:46 +0300

    feat(user_handlers.py): add import LEXICON

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
 from filters.filters import IsNumber
-from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu
+from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu, LEXICON
 from aiogram.types import (CallbackQuery,
                            Message,
                            BotCommand,
