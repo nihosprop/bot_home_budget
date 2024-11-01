6369e655927e62dc3dc856a2e34cbf92b652e66f
nihisprop <niwibonihs@tutamail.com>
2024-11-01 22:37:19 +0300

    feat(filters.py): add import Message

--- filters/filters.py
+++ filters/filters.py
@@ -1,1 +1,18 @@
 from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+
+class IsNumber(BaseFilter):
+    def __call__(self, message: Message) -> bool:
+        number = message.text
+        try:
+            int(number)
+        except ValueError:
+            try:
+                float(number)
+            except ValueError:
+                return False
+            else:
+                return True
+        else:
+            return True
