b39b72283de02be8dae89f832174443e850b75a3
nihisprop <niwibonihs@tutamail.com>
2024-11-02 00:38:02 +0300

    feat(user_handlers.py): add import create_inline_kb

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,6 +2,7 @@ import logging
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
 from filters.filters import IsNumber
+from keyboards.keyboard_utils import create_inline_kb
 from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu, LEXICON
 from aiogram.types import (CallbackQuery,
                            Message,
