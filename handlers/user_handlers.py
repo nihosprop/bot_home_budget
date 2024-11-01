0acedbcc71a4c70d4224d639a91e81178541f62f
nihisprop <niwibonihs@tutamail.com>
2024-11-01 03:22:58 +0300

    refactor(user_handlers.py):

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,12 +1,14 @@
 import logging
-from aiogram import Router, F
+from aiogram import Router, F, Bot
+from aiogram.filters import CommandStart
 from lexicon.lexicon_ru import LexiconCommandsRu
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, BotCommand
+
 
 logger = logging.getLogger(__name__)
 user_router = Router()
 
-
 @user_router.message(F.text.lower() == '/help')
 async def cmd_help(message: Message):
     await message.answer('Это команда /help')
+
