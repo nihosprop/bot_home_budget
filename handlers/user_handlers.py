53d73b3017ab0e705b26ceddca85e4a6b243e00c
nihisprop <niwibonihs@tutamail.com>
2024-11-01 22:39:09 +0300

    feat(user_handlers.py): add import IsNumber

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
-
+from filters.filters import IsNumber
 from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu
 from aiogram.types import (CallbackQuery,
                            Message,
