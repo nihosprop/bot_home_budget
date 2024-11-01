c95d22826dcf333a54c8d9321eb3ac53b1084857
nihisprop <niwibonihs@tutamail.com>
2024-11-02 01:33:50 +0300

    refactor(user_handlers.py) remove unused imports extra empty lines

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,31 +1,32 @@
 import logging
-from aiogram import Router, F, Bot
+
+from aiogram import F, Router
 from aiogram.filters import CommandStart
+from aiogram.types import (CallbackQuery, Message)
+
 from filters.filters import IsNumber
 from keyboards.keyboard_utils import create_inline_kb
-from lexicon.lexicon_ru import LexiconCommandsRu, LexiconRu, LEXICON
-from aiogram.types import (CallbackQuery,
-                           Message,
-                           BotCommand,
-                           InlineKeyboardButton,
-                           InlineKeyboardMarkup)
-
+from lexicon.lexicon_ru import LEXICON, LexiconRu
 
 logger = logging.getLogger(__name__)
 user_router = Router()
+
 
 @user_router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(LexiconRu.start)
 
+
 @user_router.message(F.text.lower() == '/help')
 async def cmd_help(message: Message):
     await message.answer(LexiconRu.help)
+
 
 @user_router.message(IsNumber())
 async def number_input(message: Message):
     keyboard = create_inline_kb(4, **LEXICON)
     await message.answer(LexiconRu.select_categories, reply_markup=keyboard)
+
 
 @user_router.callback_query(F.data.in_(LEXICON))
 async def process_button_press(callback: CallbackQuery):
