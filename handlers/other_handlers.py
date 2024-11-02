7379d09965a34d964c6b0f50f900d574d849f23e
nihisprop <niwibonihs@tutamail.com>
2024-11-02 11:55:52 +0300

    refactor(other_handlers.py): use LexiconRu.other_message in message.answer

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -7,8 +7,7 @@ from lexicon.lexicon_ru import LexiconRu
 
 logger = logging.getLogger(__name__)
 other_router = Router()
-lexicon = LexiconRu()
 
 @other_router.message()
 async def answer_to_another(message: Message):
-    await message.answer(f'{lexicon.other_message}')
+    await message.answer(f'{LexiconRu.other_message}')
