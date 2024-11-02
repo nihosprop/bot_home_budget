3949424e041d211aca8490131f973bb515db47c6
nihisprop <niwibonihs@tutamail.com>
2024-11-02 23:32:16 +0300

    feat(other_handlers.py): add imports and fsm handler

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,5 +1,7 @@
 import logging
 
+from states.states import FSMMakeTransaction
+from aiogram.filters import StateFilter
 from aiogram.types import Message
 from aiogram import Router
 from lexicon.lexicon_ru import LexiconRu
@@ -8,6 +10,6 @@ from lexicon.lexicon_ru import LexiconRu
 logger = logging.getLogger(__name__)
 other_router = Router()
 
-@other_router.message()
-async def answer_to_another(message: Message):
+@other_router.message(StateFilter(FSMMakeTransaction.fill_number))
+async def warning_not_number(message: Message):
     await message.answer(f'{LexiconRu.other_message}')
