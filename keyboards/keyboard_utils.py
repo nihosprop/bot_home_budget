55dcebbd060d13a4a589917885d292c928c5062c
nihisprop <niwibonihs@tutamail.com>
2024-11-02 17:54:30 +0300

    refactor(keyboard_utils.py)

--- keyboards/keyboard_utils.py
+++ keyboards/keyboard_utils.py
@@ -1,8 +1,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from lexicon.lexicon_ru import CATEGORY_1
+from lexicon.lexicon_ru import BUTTONS
 
-# to make universal 
 def create_inline_kb(
         width: int, *args: str, **kwargs: str) -> InlineKeyboardMarkup:
     """Generates inline keyboards on the fly"""
@@ -12,13 +11,14 @@ def create_inline_kb(
 
     if args:
         for button in args:
-            buttons.append(InlineKeyboardButton(text=CATEGORY_1[button] if
-            CATEGORY_1.get(button) else button, callback_data=button))
+            buttons.append(InlineKeyboardButton(text=BUTTONS[button] if
+            BUTTONS.get(button) else button, callback_data=button))
 
     if kwargs:
         for button, text in kwargs.items():
             buttons.append(InlineKeyboardButton(text=text, callback_data=button))
 
     kb_builder.row(*buttons, width=width)
-    kb_builder.row(InlineKeyboardButton(text='Отмена', callback_data='last_btn'))
+    kb_builder.row(InlineKeyboardButton(text=BUTTONS['cancel'],
+                                        callback_data='/cancel'))
     return kb_builder.as_markup()
