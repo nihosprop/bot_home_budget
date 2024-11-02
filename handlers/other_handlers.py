c9c9091c7445ff80a8e55ce017c6c7713c8f2c58
nihisprop <niwibonihs@tutamail.com>
2024-11-02 11:51:51 +0300

    refactor(other_handlers.py)

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -12,4 +12,4 @@ lexicon = LexiconRu()
 @other_router.message()
 async def answer_to_another(message: Message):
     user_name = message.from_user.username or message.from_user.first_name
-    await message.answer(f'{user_name} {lexicon.other_message}')
+    await message.answer(f'{lexicon.other_message}')
