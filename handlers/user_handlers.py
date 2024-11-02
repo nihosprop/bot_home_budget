696a0c21d963301d1223524d778fed836e4f929c
nihisprop <niwibonihs@tutamail.com>
2024-11-02 20:13:46 +0300

    refactor(user_handlers.py)

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,16 +1,16 @@
 import logging
-from turtledemo.penrose import start
+from typing import Any
 
 from aiogram import F, Router
 from aiogram.filters import CommandStart, StateFilter
 from aiogram.types import (CallbackQuery, Message)
 from aiogram.fsm.state import default_state
 from aiogram.fsm.context import FSMContext
+
 from filters.filters import IsNumber
 from keyboards.keyboard_utils import create_inline_kb
-from lexicon.lexicon_ru import DIRECTION, LexiconRu, GAIN_CATEGORIES
+from lexicon.lexicon_ru import DIRECTION, GAIN_CATEGORIES, LexiconRu
 from states.states import FSMMakeTransaction
-
 
 logger = logging.getLogger(__name__)
 user_router = Router()
@@ -20,26 +20,30 @@ async def cmd_start(message: Message, state: FSMContext):
     await message.answer(LexiconRu.start)
     await state.set_state(FSMMakeTransaction.fill_number)
 
+
 @user_router.callback_query(F.data == '/cancel', ~StateFilter(default_state))
-async def process_cancel_command_state(callback: CallbackQuery,
-                                       state: FSMContext):
+async def process_cancel_command_state(
+        callback: CallbackQuery, state: FSMContext):
     await callback.message.delete()
     await callback.message.answer(LexiconRu.waiting_number)
     await state.set_state(FSMMakeTransaction.fill_number)
 
+
 @user_router.message(StateFilter(FSMMakeTransaction.fill_number), IsNumber())
 async def process_number_sent(message: Message, state: FSMContext):
-    keyboard = create_inline_kb(4, **DIRECTION)
+    keyboard = create_inline_kb(2, **DIRECTION)
     await state.update_data(amount=int(message.text))
     await message.answer(LexiconRu.select_direction, reply_markup=keyboard)
     await state.set_state(FSMMakeTransaction.select_direction)
 
+
 @user_router.callback_query(StateFilter(FSMMakeTransaction.select_direction),
                             F.data.in_(DIRECTION))
-async def process_button_press(callback: CallbackQuery, state: FSMContext):
-    keyboard = create_inline_kb(4, **GAIN_CATEGORIES)
+async def process_button_press_direction(
+        callback: CallbackQuery, state: FSMContext):
+    keyboard = create_inline_kb(2, **GAIN_CATEGORIES)
     await callback.message.edit_text(LexiconRu.select_category,
-                                  reply_markup=keyboard)
+                                     reply_markup=keyboard)
     await callback.answer()
     await state.set_state(FSMMakeTransaction.select_category)
 
