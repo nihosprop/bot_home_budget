fabc36e7c79373e66ffec29412d088fca2f8a077
nihisprop <niwibonihs@tutamail.com>
2024-11-03 13:21:35 +0300

    refactor(user_handlers.py): change some handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -27,18 +27,26 @@ async def process_cancel_command_state(
         callback: CallbackQuery, state: FSMContext):
     await callback.message.delete()
     await callback.message.answer(LexiconRu.waiting_number)
+    await callback.answer()
     await state.set_state(FSMMakeTransaction.fill_number)
 
 
 # fill_number
 @user_router.message(StateFilter(FSMMakeTransaction.fill_number), IsNumber())
-async def process_number_sent(message: Message, state: FSMContext):
+async def process_number_sent(
+        message: Message, state: FSMContext, number: dict[str, int | float]):
     keyboard = create_inline_kb(2, **DIRECTION)
-    await state.update_data(amount=int(message.text))
+    await state.update_data(amount=number)
     await message.answer(LexiconRu.select_direction, reply_markup=keyboard)
     await state.set_state(FSMMakeTransaction.select_direction)
 
 
+@user_router.message(StateFilter(FSMMakeTransaction.fill_number))
+async def process_invalid_number(message: Message):
+    await message.answer(f'{LexiconRu.other_message}')
+
+
+# select_direction
 @user_router.callback_query(StateFilter(FSMMakeTransaction.select_direction),
                             F.data == 'gain')
 async def button_press_gain(
@@ -50,6 +58,13 @@ async def button_press_gain(
     await state.set_state(FSMMakeTransaction.select_category)
 
 
+@user_router.message(StateFilter(FSMMakeTransaction.select_direction))
+async def process_invalid_select_direction(message: Message, state: FSMContext):
+    # остается сообщение 'Выберите направление' надо его убрать
+    await message.answer(text='Выберите направление')
+
+
+# select_category
 @user_router.callback_query(StateFilter(FSMMakeTransaction.select_category),
                             F.data.in_(GAIN_CATEGORIES))
 async def press_bt_gain_categories(callback: CallbackQuery, state: FSMContext):
@@ -65,4 +80,10 @@ async def press_bt_gain_categories(callback: CallbackQuery, state: FSMContext):
     await callback.message.edit_text(f'{LexiconRu.transaction_recorded}\n'
                                      f'{LexiconRu.waiting_number}')
     logger.info(f'{user_dict}')
+    await callback.answer()
     await state.set_state(FSMMakeTransaction.fill_number)
+
+
+@user_router.message(StateFilter(FSMMakeTransaction.select_category))
+async def process_invalid_gain_categories(message: Message):
+    await message.answer(text='Выберите категорию')
