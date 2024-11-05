5ffa3e7b3a3dbd6d50c39b02133d5e45e54eb71b
nihisprop <niwibonihs@tutamail.com>
2024-11-05 23:40:16 +0300

    refactor: update name keyboard

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,5 @@
 from keyboards.keyboard_utils import create_inline_kb
 from lexicon.lexicon_ru import GAIN_CATEGORIES, DIRECTION
 
-kb_for_gain = create_inline_kb(2, **GAIN_CATEGORIES)
+kb_gain_categories = create_inline_kb(2, **GAIN_CATEGORIES)
 kb_direction = create_inline_kb(2, **DIRECTION)
