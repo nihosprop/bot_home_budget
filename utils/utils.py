31ce01c4bda487e4fcffe4f85d1d2ad1f1fc3c8a
nihisprop <niwibonihs@tutamail.com>
2024-11-06 21:31:08 +0300

    feat: add add_income_data_in_db
    The function enters user income data into the database

--- utils/utils.py
+++ utils/utils.py
@@ -6,3 +6,13 @@ from database.db import database
 
 logger = logging.getLogger(__name__)
 
+async def add_income_data_in_db(clbk: CallbackQuery,
+                          state: FSMContext,
+                          db=database):
+    category = clbk.data
+    user_id = str(clbk.from_user.id)
+    data = await state.get_data()
+    amount = data['amount']
+    db.setdefault(user_id, {'income': {}, 'expenses': {}})
+    db[user_id]['income'][category] = (
+            db[user_id]['income'].setdefault(category, 0) + amount)
