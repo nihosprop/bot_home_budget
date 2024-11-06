d0830252b9c96e3cfb1844f291affc1f953fc1b0
nihisprop <niwibonihs@tutamail.com>
2024-11-07 00:48:55 +0300

    fix(FSMMakeTransaction): update
    State select_category expanded to select_income and select_expenses

--- states/states.py
+++ states/states.py
@@ -4,5 +4,6 @@ from aiogram.fsm.state import StatesGroup, State
 class FSMMakeTransaction(StatesGroup):
     fill_number = State()
     select_direction = State()
-    select_category = State()
+    select_income = State()
+    select_expenses = State()
     select_subcategory = State()
