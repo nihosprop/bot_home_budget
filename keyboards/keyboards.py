a1b97cb6cee5eda116ab204e9731b3573f60c370
nihisprop <niwibonihs@tutamail.com>
2024-11-06 22:03:24 +0300

    feat: add import EXPENSES_CATEGORIES, add kb_expenses_categories

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,6 @@
 from keyboards.keyboard_utils import create_inline_kb
-from lexicon.lexicon_ru import GAIN_CATEGORIES, DIRECTION
+from lexicon.lexicon_ru import GAIN_CATEGORIES, DIRECTION, EXPENSES_CATEGORIES
 
 kb_gain_categories = create_inline_kb(2, **GAIN_CATEGORIES)
+kb_expenses_categories = create_inline_kb(2, **EXPENSES_CATEGORIES)
 kb_direction = create_inline_kb(2, **DIRECTION)
