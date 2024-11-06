cbe7e97e4d18bc91041a1502df8f9dff6be6d8ec
nihisprop <niwibonihs@tutamail.com>
2024-11-07 00:50:40 +0300

    feat: add add_expenses_in_db

--- utils/utils.py
+++ utils/utils.py
@@ -16,3 +16,14 @@ async def add_income_in_db(clbk: CallbackQuery,
     db.setdefault(user_id, {'income': {}, 'expenses': {}})
     db[user_id]['income'][category] = (
             db[user_id]['income'].setdefault(category, 0) + amount)
+
+async def add_expenses_in_db(clbk: CallbackQuery,
+                           state: FSMContext,
+                           db=database):
+    category = clbk.data
+    user_id = str(clbk.from_user.id)
+    data = await state.get_data()
+    amount = data['amount']
+    db.setdefault(user_id, {'income': {}, 'expenses': {}})
+    db[user_id]['expenses'][category] = (
+            db[user_id]['expenses'].setdefault(category, 0) + amount)
