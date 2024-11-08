9b4b56e92b98b0509f9091f2d11b913cf63b5ec3
nihisprop <niwibonihs@tutamail.com>
2024-11-08 12:52:50 +0300

    refactor

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,7 +1,5 @@
 import logging
 
-from states.states import FSMMakeTransaction
-from aiogram.filters import StateFilter
 from aiogram.types import Message
 from aiogram import Router
 from lexicon.lexicon_ru import LexiconRu
@@ -12,4 +10,4 @@ other_router = Router()
 
 @other_router.message()
 async def other_message(msg: Message):
-    await msg.answer('Что-то пошло не так. Нажмите\n/start')
+    await msg.answer(LexiconRu.problems)
