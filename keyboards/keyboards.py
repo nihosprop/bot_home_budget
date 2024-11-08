ad2499742146c8039efc2e04f495d6d90380ec56
nihisprop <niwibonihs@tutamail.com>
2024-11-08 19:11:40 +0300

    refactor: use kb_income_categories

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,6 +1,6 @@
 from keyboards.keyboard_utils import create_inline_kb
 from lexicon.lexicon_ru import INCOME_CATEGORIES, DIRECTION, EXPENSES_CATEGORIES
 
-kb_gain_categories = create_inline_kb(2, **INCOME_CATEGORIES)
+kb_income_categories = create_inline_kb(2, **INCOME_CATEGORIES)
 kb_expenses_categories = create_inline_kb(2, **EXPENSES_CATEGORIES)
 kb_direction = create_inline_kb(2, **DIRECTION)
