526a2cca9eb0d717f8ee738fd151a21ca27ff3d1
nihisprop <niwibonihs@tutamail.com>
2024-11-15 12:33:01 +0300

    feat: add generate_fin_report
    Added a function for generating a transaction report, as well as related imports

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,10 @@
 import logging
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
+
+from lexicon.lexicon_ru import (EXPENSES_CATEG_BUTT,
+                                EXPENSE_SUBCATEGORY_BUTTONS,
+                                INCOME_CATEG_BUTT)
 from database.db import database as db
 
 logger_utils = logging.getLogger(__name__)
@@ -29,4 +33,35 @@ async def add_expenses_in_db(clbk: CallbackQuery,
     db.setdefault(user_id, {'income': {}, 'expenses': {}})
     db[user_id]['expenses'][category][subcategory] = (
             db[user_id]['expenses'].setdefault(category, {}).setdefault(
-                    subcategory, 0) + amount)
+                    subcategory,
+                    0) + amount)
+
+
+async def generate_fin_report(msg: Message, data: dict) -> str:
+    user_id = str(msg.from_user.id)
+    balance = 0
+    monthly_income: dict[str, float | int] = data[user_id]['income']
+    sum_income = sum(monthly_income.values())
+
+    expenses: dict[str, dict[str, float | int]] = data[user_id]['expenses']
+    sum_expenses = sum(
+            sum(obj) for obj in (categ.values() for categ in expenses.values()))
+
+    report: str = (f'<b>Баланс: {balance + sum_income}\n'
+                   f'Сальдо:'
+                   f' {sum_income - sum_expenses}\n'
+                   f'------------------------\n'
+                   f'Доходы за месяц: {sum_income}</b>\n')
+
+    for categ, value in monthly_income.items():
+        report += f'  {INCOME_CATEG_BUTT[categ]}: {value}\n'
+    report += (f'------------------------\n'
+               f'<b>Расходы за месяц: {round(sum_expenses, 2)}<b>\n')
+
+    for category, data in expenses.items():
+        report += f'  {EXPENSES_CATEG_BUTT[category]}:\n'
+        for subcategory, value in data.items():
+            report += (f'    {EXPENSE_SUBCATEGORY_BUTTONS[subcategory]}: '
+                       f'{value}\n')
+    report + '\nПришлите сумму…'
+    return report
