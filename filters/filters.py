b8ae0aaf021b57a5f5147137dcb858260d6a8678
nihisprop <niwibonihs@tutamail.com>
2024-11-25 23:27:53 +0300

    fix(IsNumber): update
    Change log level from info to error

--- filters/filters.py
+++ filters/filters.py
@@ -30,8 +30,8 @@ class IsNumber(BaseFilter):
 
                     value = {'number': float(number)}
                 except ValueError:
-                    logger_filters.info(f'Failed to convert message to number:'
-                                        f' {message.text}')
+                    logger_filters.error(f'Failed to convert message to number:'
+                                         f' {message.text=}')
                     return False
             logger_filters.debug(f'{value=}')
             return value if value['number'] != 0 else False
