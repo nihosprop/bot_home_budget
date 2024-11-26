5865cefd41538304a7eb3e38a910fa4aee621899
nihisprop <niwibonihs@tutamail.com>
2024-11-26 21:35:17 +0300

    feat: add class FSMDeleteUser and confirm_deletion state

--- states/states.py
+++ states/states.py
@@ -10,3 +10,6 @@ class FSMMakeTransaction(StatesGroup):
     select_income = State()
     select_expenses = State()
     select_subcategory = State()
+
+class FSMDeleteUser(StatesGroup):
+    confirm_deletion = State()
