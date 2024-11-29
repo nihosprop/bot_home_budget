64af78c6e8536feb498a0b1676afc7a10e4cdd3a
nihisprop <niwibonihs@tutamail.com>
2024-11-29 19:15:06 +0300

    refactor

--- filters/filters.py
+++ filters/filters.py
@@ -18,20 +18,22 @@ class IsNumber(BaseFilter):
         """
         if message.content_type is ContentType.TEXT:
             number = message.text.replace(',', '.')
+            user_id = message.from_user.id
             try:
                 value = {'number': int(number)}
             except ValueError:
                 try:
                     if not math.isfinite(float(number)):
-                        user_id = message.from_user.id
+
                         logger_filters.warning(f'NaN or INF attempt!!! ->'
-                                               f' {user_id=}:{number=}')
+                                               f' {user_id=}: {number=}')
                         return False
 
                     value = {'number': float(number)}
                 except ValueError:
-                    logger_filters.warning(f'Failed to convert message to number:'
-                                         f' {message.text=}')
+                    logger_filters.warning(
+                        f'Failed to convert message to number:'
+                        f' {user_id=}: {message.text=}')
                     return False
             logger_filters.debug(f'{value=}')
             return value if value['number'] != 0 else False
