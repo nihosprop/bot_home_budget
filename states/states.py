9ebc783c6a86300162963ffbfd9cdfd96acc4af6
nihisprop <niwibonihs@tutamail.com>
2024-12-10 23:49:30 +0300

    refactor(FSMMakeTransaction): del state fill_number

--- states/states.py
+++ states/states.py
@@ -5,7 +5,6 @@ from aiogram.fsm.state import StatesGroup, State
 logger_states = logging.getLogger(__name__)
 
 class FSMMakeTransaction(StatesGroup):
-    fill_number = State()
     select_direction = State()
     select_income = State()
     select_expenses = State()
