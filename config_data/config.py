80f2d82493b3af3fdf75553d959cade3f5d7d5bc
nihisprop <niwibonihs@tutamail.com>
2024-12-25 21:54:32 +0300

    chore: add id_admin

--- config_data/config.py
+++ config_data/config.py
@@ -4,6 +4,7 @@ from environs import Env
 @dataclass
 class TgBot:
     token: str
+    id_admin: str
 
 @dataclass
 class Config:
@@ -12,4 +13,4 @@ class Config:
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'), id_admin=env('ID_ADMIN')))
