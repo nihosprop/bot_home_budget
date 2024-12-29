2efcb38f72db430c1c63522d62ea149de1ff4f6d
nihisprop <niwibonihs@tutamail.com>
2024-12-29 16:21:36 +0300

    feat(IsAdmin): add logging lv DEBUG

--- filters/filters.py
+++ filters/filters.py
@@ -42,6 +42,9 @@ class IsNumber(BaseFilter):
 
 class IsAdmin(BaseFilter):
     async def __call__(self, msg: Message, superadmin) -> bool:
+        logger_filters.debug('Entry')
         user_id = str(msg.from_user.id)
-        logger_filters.debug(f'{user_id=}:{superadmin=}')
+        logger_filters.debug(f'In {__class__.__name__}:{user_id=}'
+                             f':{superadmin=}\n{user_id == superadmin=}')
+        logger_filters.debug('Exit')
         return user_id == superadmin
